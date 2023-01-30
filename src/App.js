a77c8d4b1f3053ab9840c234d49506ecdab7e1b9
thijsBoet <m.boet@chello.nl>
2023-01-30 14:33:59 +0100

    start building panel component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import { useState } from 'react';
 import AccordionPage from './pages/AccordionPage';
 import ButtonPage from './pages/ButtonPage';
 import Dropdown from './components/Dropdown';
 
 const App = () => {
+	const [selection, setSelection] = useState(null);
+
+	const handleSelect = option => {
+		setSelection(option);
+	};
+
 	const options = [
 		{ label: 'The Color Red', value: 'red' },
 		{ label: 'The Color Green', value: 'green' },
 		{ label: 'A Shade of Blue', value: 'blue' },
-	]
+	];
 
-	return <Dropdown options={options} />;
+	return (
+		<Dropdown options={options} value={selection} onChange={handleSelect} />
+	);
 };
 
 export default App;
